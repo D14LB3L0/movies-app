2300d0e307ab5b95061753e47a49c6cbff1d6083
D14LB3L0 <diegobdev2024@gmail.com>
2025-07-06 13:22:26 +0100

    detils

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-   
   const DetailsScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
-    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    return Scaffold(body: CustomScrollView(slivers: [_CustomAppBar()]));
+  }
+}
 
-    return Scaffold(
-      body: Container(
-        child: Center(  
-            child: Text('$movie'),
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 100,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          color: Colors.black12,
+          alignment: Alignment.bottomCenter,
+          child: Text('movie.title', style: TextStyle(fontSize: 16, color: Colors.white),),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://picsum.photos/500/300'),
+          fit: BoxFit.cover,
         ),
       ),
     );
